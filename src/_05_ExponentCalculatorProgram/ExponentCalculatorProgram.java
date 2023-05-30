7fd98773ea79a747c12da784b3d5ece044a75544
ChatGTHB <cihat.kose@hotmail.com>
2023-05-30 11:50:24 +0300

    _05_ExponentCalculatorProgram updated

--- src/_05_ExponentCalculatorProgram/ExponentCalculatorProgram.java
+++ src/_05_ExponentCalculatorProgram/ExponentCalculatorProgram.java
@@ -6,7 +6,20 @@ public class ExponentCalculatorProgram {
 
     public static void main(String[] args) {
 
-        int base = 0, exponent = 0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number to be exponentiated : ");
+        int base = input.nextInt();
+
+        System.out.print("Enter the number to be the exponent : ");
+        int exponent = input.nextInt();
+
+        if (exponent < 0) {
+            while (exponent < 0) {
+                System.out.print("Please enter a non-negative number : ");
+                exponent = input.nextInt();
+            }
+        }
 
         calculateExponent(base, exponent);
 
@@ -16,26 +29,11 @@ public class ExponentCalculatorProgram {
 
         int total = 1;
 
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter the number to be exponentiated : ");
-        base = input.nextInt();
-
-        System.out.print("Enter the number to be the exponent : ");
-        exponent = input.nextInt();
-
-        if (exponent < 0) {
-            while (exponent < 0) {
-                System.out.print("Please enter a non-negative number : ");
-                exponent = input.nextInt();
-            }
-        }
-
         for (int i = 1; i <= exponent; i++) {
             total *= base;
         }
 
-        System.out.println("Total = " + total);
+        System.out.println(base + "^" + exponent + " = " + total);
     }
 }
 
