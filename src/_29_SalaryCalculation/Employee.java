09b7027c0d2ef6022423d74d925da4974b1409e7
ChatGTHB <cihat.kose@hotmail.com>
2023-05-30 18:42:43 +0300

    _29_SalaryCalculation added

---
+++ src/_29_SalaryCalculation/Employee.java
@@ -0,0 +1,61 @@
+package _29_SalaryCalculation;
+
+public class Employee {
+    String name;
+    double salary;
+    int workHours;
+    int hireYear;
+    double tax;
+    double bonus;
+    double raiseSalary;
+
+
+    public Employee(String name, double salary, int workHours, int hireYear) {
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+        this.tax = tax();
+        this.bonus = bonus();
+        this.raiseSalary = raiseSalary();
+    }
+
+    public double tax() {
+        if (this.salary > 1000) {
+            this.tax = this.salary * ((double) 3 / 100);
+        }
+        return this.tax;
+    }
+
+    public double bonus() {
+        if (this.workHours > 40) {
+            this.bonus = (this.workHours - 40) * 30;
+        }
+        return this.bonus;
+    }
+
+    public double raiseSalary() {
+        if (this.hireYear > 2001) {
+            this.raiseSalary = this.salary * ((double) 5 / 100);
+        } else if (this.hireYear < 2001 && this.hireYear > 1991) {
+            this.raiseSalary = this.salary * ((double) 1 / 10);
+        } else if (this.hireYear <= 1991) {
+            this.raiseSalary = this.salary * ((double) 15 / 100);
+        }
+        return this.raiseSalary;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Name = " + this.name + "\n" +
+                "Salary = " + this.salary + "\n" +
+                "Work Hours = " + this.workHours + "\n" +
+                "Hire Year = " + this.hireYear + "\n" +
+                "Tax = " + this.tax + "\n" +
+                "Bonus = " + this.bonus + "\n" +
+                "Raise Salary = " + this.raiseSalary + "\n" +
+                "Salary without raise with taxes and bonuses = " + (this.salary-this.tax+this.bonus) + "\n" +
+                "Zamlı Maaş = " + (this.salary+this.raiseSalary);
+    }
+}
---
+++ src/_29_SalaryCalculation/SalaryCalculation.java
@@ -0,0 +1,10 @@
+package _29_SalaryCalculation;
+
+public class SalaryCalculation {
+    public static void main(String[] args) {
+
+        Employee employee=new Employee("Said",2000.0,45,1985);
+        System.out.println(employee);
+
+    }
+}
